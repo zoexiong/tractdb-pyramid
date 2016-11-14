85987497714ae0c833ab6400d0783640d3e3e03a
zoexiong <xiongsy@uw.edu>
2016-11-13 21:50:55 -0800

    change url to localhost

--- tests/test_accountview.py
+++ tests/test_accountview.py
@@ -5,79 +5,79 @@ TEST_ACCOUNT = 'testaccountview_account'
 TEST_ACCOUNT_PASSWORD = 'testaccountview_password'
 TEST_ROLE = 'testaccountview-role'
 
-ACCOUNT_URL = 'http://192.168.99.100:8080/account'
-ACCOUNT_COLLECTION_URL = 'http://192.168.99.100:8080/accounts'
-ROLE_COLLECTION_URL = 'http://192.168.99.100:8080/account/'+TEST_ACCOUNT+'/roles'
-ROLE_URL = 'http://192.168.99.100:8080/account/'+TEST_ACCOUNT+'/roles/'+TEST_ROLE
+ACCOUNT_URL = 'http://localhost:8080/account'
+ACCOUNT_COLLECTION_URL = 'http://localhost:8080/accounts'
+ROLE_COLLECTION_URL = 'http://localhost:8080/account/'+TEST_ACCOUNT+'/roles'
+ROLE_URL = 'http://localhost:8080/account/'+TEST_ACCOUNT+'/roles/'+TEST_ROLE
 
 
 class TestAccountView(unittest.TestCase):
-    # def test_create_and_delete_account(self):
-    #     # Get the current accounts
-    #     r = requests.get(
-    #         ACCOUNT_COLLECTION_URL,
-    #     )
-    #     self.assertEqual(r.status_code, 200)
-    #
-    #     # Ensure it does not already exist (this could fail silently if the account doesn't exist)
-    #     r = requests.delete(
-    #         '{}/{}'.format(
-    #             ACCOUNT_URL,
-    #             TEST_ACCOUNT
-    #         )
-    #     )
-    #
-    #     # Create the account
-    #     r = requests.post(
-    #         ACCOUNT_COLLECTION_URL,
-    #         json={
-    #             'account': TEST_ACCOUNT,
-    #             'password': TEST_ACCOUNT_PASSWORD
-    #         }
-    #     )
-    #     self.assertEqual(r.status_code, 201)
-    #
-    #     # Test the creating it again fails
-    #     r = requests.post(
-    #         ACCOUNT_COLLECTION_URL,
-    #         json={
-    #             'account': TEST_ACCOUNT,
-    #             'password': TEST_ACCOUNT_PASSWORD
-    #         }
-    #     )
-    #     self.assertEqual(r.status_code, 409)
-    #
-    #     # Test the account exists
-    #     r = requests.get(
-    #         ACCOUNT_COLLECTION_URL,
-    #     )
-    #     self.assertEqual(r.status_code, 200)
-    #     self.assertIn(TEST_ACCOUNT, r.json()['accounts'])
-    #
-    #     # Delete the account
-    #     r = requests.delete(
-    #         '{}/{}'.format(
-    #             ACCOUNT_URL,
-    #             TEST_ACCOUNT
-    #         )
-    #     )
-    #     self.assertEqual(r.status_code, 200)
-    #
-    #     # Test that deleting the account again fails
-    #     r = requests.delete(
-    #         '{}/{}'.format(
-    #             ACCOUNT_URL,
-    #             TEST_ACCOUNT
-    #         )
-    #     )
-    #     self.assertEqual(r.status_code, 404)
-    #
-    #     # Test the account is gone
-    #     r = requests.get(
-    #         ACCOUNT_COLLECTION_URL,
-    #     )
-    #     self.assertEqual(r.status_code, 200)
-    #     self.assertNotIn(TEST_ACCOUNT, r.json()['accounts'])
+    def test_create_and_delete_account(self):
+        # Get the current accounts
+        r = requests.get(
+            ACCOUNT_COLLECTION_URL,
+        )
+        self.assertEqual(r.status_code, 200)
+
+        # Ensure it does not already exist (this could fail silently if the account doesn't exist)
+        r = requests.delete(
+            '{}/{}'.format(
+                ACCOUNT_URL,
+                TEST_ACCOUNT
+            )
+        )
+
+        # Create the account
+        r = requests.post(
+            ACCOUNT_COLLECTION_URL,
+            json={
+                'account': TEST_ACCOUNT,
+                'password': TEST_ACCOUNT_PASSWORD
+            }
+        )
+        self.assertEqual(r.status_code, 201)
+
+        # Test the creating it again fails
+        r = requests.post(
+            ACCOUNT_COLLECTION_URL,
+            json={
+                'account': TEST_ACCOUNT,
+                'password': TEST_ACCOUNT_PASSWORD
+            }
+        )
+        self.assertEqual(r.status_code, 409)
+
+        # Test the account exists
+        r = requests.get(
+            ACCOUNT_COLLECTION_URL,
+        )
+        self.assertEqual(r.status_code, 200)
+        self.assertIn(TEST_ACCOUNT, r.json()['accounts'])
+
+        # Delete the account
+        r = requests.delete(
+            '{}/{}'.format(
+                ACCOUNT_URL,
+                TEST_ACCOUNT
+            )
+        )
+        self.assertEqual(r.status_code, 200)
+
+        # Test that deleting the account again fails
+        r = requests.delete(
+            '{}/{}'.format(
+                ACCOUNT_URL,
+                TEST_ACCOUNT
+            )
+        )
+        self.assertEqual(r.status_code, 404)
+
+        # Test the account is gone
+        r = requests.get(
+            ACCOUNT_COLLECTION_URL,
+        )
+        self.assertEqual(r.status_code, 200)
+        self.assertNotIn(TEST_ACCOUNT, r.json()['accounts'])
 
     def test_create_and_delete_role(self):
         # Get the current accounts
--- tests/test_authenticationviews.py
+++ tests/test_authenticationviews.py
@@ -1,105 +1,105 @@
-# import tests.docker_base as docker_base
-# import requests
-# import tractdb.server.accounts
-# import unittest
-# import yaml
-#
-# LOGIN_URL = 'http://192.168.99.100:8080/login'
-# AUTHENTICATED_URL = 'http://192.168.99.100:8080/authenticated'
-# LOGOUT_URL = 'http://192.168.99.100:8080/logout'
-#
-# TEST_ACCOUNT = 'testauthenticationviews_account'
-# TEST_ACCOUNT_PASSWORD = 'testauthenticationviews_password'
-#
-#
-# def setup():
-#     # Parse our couchdb secrets
-#     with open('tests/test-secrets/couchdb_secrets.yml') as f:
-#         couchdb_secrets = yaml.safe_load(f)
-#
-#     # Create our admin object
-#     admin = tractdb.server.accounts.AccountsAdmin(
-#         couchdb_url='http://{}:5984'.format(
-#             docker_base.ip()
-#         ),
-#         couchdb_admin=couchdb_secrets['admin']['user'],
-#         couchdb_admin_password=couchdb_secrets['admin']['password']
-#     )
-#
-#     # Create the account we expect
-#     if TEST_ACCOUNT not in admin.list_accounts():
-#         admin.create_account(TEST_ACCOUNT, TEST_ACCOUNT_PASSWORD)
-#
-#
-# def teardown():
-#     pass
-#
-#
-# class TestAuthenticationViews(unittest.TestCase):
-#     def test_authenticated(self):
-#         response = requests.get(
-#             AUTHENTICATED_URL
-#         )
-#
-#         self.assertEqual(response.status_code, 403)
-#
-#     def test_login(self):
-#         session = requests.Session()
-#
-#         response = session.post(
-#             LOGIN_URL,
-#             json={
-#                 'account': TEST_ACCOUNT,
-#                 'password': TEST_ACCOUNT_PASSWORD
-#             }
-#         )
-#         self.assertEqual(response.status_code, 200)
-#
-#         response = session.get(
-#             AUTHENTICATED_URL
-#         )
-#         self.assertEqual(response.status_code, 200)
-#
-#     def test_login_fail(self):
-#         session = requests.Session()
-#
-#         response = session.post(
-#             LOGIN_URL,
-#             json={
-#                 'account': TEST_ACCOUNT,
-#                 'password': 'invalid_password'
-#             }
-#         )
-#         self.assertEqual(response.status_code, 401)
-#
-#     def test_logout(self):
-#         session = requests.Session()
-#
-#         response = session.get(
-#             AUTHENTICATED_URL
-#         )
-#         self.assertEqual(response.status_code, 403)
-#
-#         response = session.post(
-#             LOGIN_URL,
-#             json={
-#                 'account': TEST_ACCOUNT,
-#                 'password': TEST_ACCOUNT_PASSWORD
-#             }
-#         )
-#         self.assertEqual(response.status_code, 200)
-#
-#         response = session.get(
-#             AUTHENTICATED_URL
-#         )
-#         self.assertEqual(response.status_code, 200)
-#
-#         response = session.post(
-#             LOGOUT_URL
-#         )
-#         self.assertEqual(response.status_code, 200)
-#
-#         response = session.get(
-#             AUTHENTICATED_URL
-#         )
-#         self.assertEqual(response.status_code, 403)
+import tests.docker_base as docker_base
+import requests
+import tractdb.server.accounts
+import unittest
+import yaml
+
+LOGIN_URL = 'http://localhost:8080/login'
+AUTHENTICATED_URL = 'http://localhost:8080/authenticated'
+LOGOUT_URL = 'http://localhost:8080/logout'
+
+TEST_ACCOUNT = 'testauthenticationviews_account'
+TEST_ACCOUNT_PASSWORD = 'testauthenticationviews_password'
+
+
+def setup():
+    # Parse our couchdb secrets
+    with open('tests/test-secrets/couchdb_secrets.yml') as f:
+        couchdb_secrets = yaml.safe_load(f)
+
+    # Create our admin object
+    admin = tractdb.server.accounts.AccountsAdmin(
+        couchdb_url='http://{}:5984'.format(
+            docker_base.ip()
+        ),
+        couchdb_admin=couchdb_secrets['admin']['user'],
+        couchdb_admin_password=couchdb_secrets['admin']['password']
+    )
+
+    # Create the account we expect
+    if TEST_ACCOUNT not in admin.list_accounts():
+        admin.create_account(TEST_ACCOUNT, TEST_ACCOUNT_PASSWORD)
+
+
+def teardown():
+    pass
+
+
+class TestAuthenticationViews(unittest.TestCase):
+    def test_authenticated(self):
+        response = requests.get(
+            AUTHENTICATED_URL
+        )
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_login(self):
+        session = requests.Session()
+
+        response = session.post(
+            LOGIN_URL,
+            json={
+                'account': TEST_ACCOUNT,
+                'password': TEST_ACCOUNT_PASSWORD
+            }
+        )
+        self.assertEqual(response.status_code, 200)
+
+        response = session.get(
+            AUTHENTICATED_URL
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_fail(self):
+        session = requests.Session()
+
+        response = session.post(
+            LOGIN_URL,
+            json={
+                'account': TEST_ACCOUNT,
+                'password': 'invalid_password'
+            }
+        )
+        self.assertEqual(response.status_code, 401)
+
+    def test_logout(self):
+        session = requests.Session()
+
+        response = session.get(
+            AUTHENTICATED_URL
+        )
+        self.assertEqual(response.status_code, 403)
+
+        response = session.post(
+            LOGIN_URL,
+            json={
+                'account': TEST_ACCOUNT,
+                'password': TEST_ACCOUNT_PASSWORD
+            }
+        )
+        self.assertEqual(response.status_code, 200)
+
+        response = session.get(
+            AUTHENTICATED_URL
+        )
+        self.assertEqual(response.status_code, 200)
+
+        response = session.post(
+            LOGOUT_URL
+        )
+        self.assertEqual(response.status_code, 200)
+
+        response = session.get(
+            AUTHENTICATED_URL
+        )
+        self.assertEqual(response.status_code, 403)
